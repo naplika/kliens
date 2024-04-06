5723cbd7e27c58e919f24e8cb3739d3a6b18c4e2
BalazsManus <contact@balazsmanus.hu>
2024-04-06 20:01:29 +0200

    add basic shit

--- kretamechanics.vb
+++ kretamechanics.vb
@@ -1,3 +1,12 @@
+Imports System.Net.Http
+imports kliens.SharedElements
 Public Class Kretamechanics
-    
+    dim Client = new HttpClient()
+    dim schoolspath as string = GetStartupPath() + "schools.json"
+    public function SearchSchool(query as string) as Task
+        dim task as task = task.Run(Sub()
+            
+            end sub)
+        return task
+    End function
 End Class
