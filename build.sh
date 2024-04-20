459034bcfd45559ec52a4c8f06c7e3f47c674f0b
Your Name <you@example.com>
2024-04-20 11:53:15 +0200

    change build script no to include the fucking whole path

--- build.sh
+++ build.sh
@@ -5,10 +5,16 @@ rm -r build/
 mkdir build/
 
 dotnet publish --no-dependencies --self-contained true --os linux
-zip -r build/linux-x64.zip bin/Release/net8.0/linux-x64/publish/
+cd bin/Release/net8.0/linux-x64/publish/
+zip -r ../../../../../build/linux-x64.zip .
+cd -
 
 dotnet publish --no-dependencies --self-contained true --os osx
-zip -r build/osx-x64.zip bin/Release/net8.0/osx-x64/publish/
+cd bin/Release/net8.0/osx-x64/publish/
+zip -r ../../../../../build/osx-x64.zip .
+cd -
 
 dotnet publish --no-dependencies --self-contained true --os win
-zip -r build/win-x64.zip bin/Release/net8.0/win-x64/publish/
+cd bin/Release/net8.0/win-x64/publish/
+zip -r ../../../../../build/win-x64.zip .
+cd -
